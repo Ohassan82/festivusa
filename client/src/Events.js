294fd9376f48330cda5e63b2d3694a54211a080b
Nick Willbanks <nwillbanks@gmail.com>
2019-03-02 10:33:36 -0600

    map update, added cards

--- client/src/Events.js
+++ client/src/Events.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 import Search from './Search';
+import Card from 'react-bootstrap/Card';
+import CardColumns from 'react-bootstrap/CardColumns'
 
 class Events extends Component {
     constructor(props){
@@ -49,14 +51,21 @@ render() {
         handleSearch= {e => this.handleSearch(e)} 
         />
         <div>
-        <ul>
+        <CardColumns>
           {events.map(event => (
-            <li key={event.id}>
-            <h1>{event.title}</h1>
-            <h1>{event.description}</h1>
-            </li>
+            <Card className="p-3" key={event.id} bg="primary" text="white" style={{ width: '18rem' }}>
+                <Card.Body>
+                    <Card.Title>{event.title}</Card.Title>
+                    <Card.Subtitle className="mb-2 text-muted">{event.country}</Card.Subtitle>
+                    <Card.Text>
+                    
+                    </Card.Text>
+                    <Card.Link href="#">More Details</Card.Link>
+                    <Card.Link href="#"> Map</Card.Link>
+                </Card.Body>
+            </Card>
           ))}
-          </ul>
+          </CardColumns>
         </div>
     </div>
     );
