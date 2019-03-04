810dd10d3ee2235beabbac21b667156ded8d8160
Sam Matthiae <rocketsam13@yahoo.com>
2019-03-04 14:01:26 -0600

    cleaned up file tree, still working on building sign in front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import Login from "./component/login";
+import Form from "./component/login";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends Component {
   render() {
     return (
-      <Login />
+      <Login />,
+      <Form />
+
     );
   }
 }
