6c0391dcc87c2f26259cef1e09383f7ce24b5fea
Sam Matthiae <rocketsam13@yahoo.com>
2019-03-04 18:40:59 -0600

    just saving so i can change branch

--- client/src/component/login/Login.js
+++ client/src/component/login/Login.js
@@ -1,18 +1,35 @@
 import React, { Component } from "react";
+import { Container, NavbarBrand, Nav, NavItem } from "reactstrap";
+import { NavLink } from 'react-router-dom';
+import { Navbar } from 'react-bootstrap';
+         
+  
+   
 
 
 
 class Login extends Component {
+
+    state = {
+
+    }
+
+
     render() {
         return (
+            <div className="Login">
+                 <Navbar color="dark" dark expand="sm" className="mb-5">
                     <Container>
                         <NavbarBrand href="/">FESTIVUS ** Get together **</NavbarBrand>
                         <Nav className="ml-auto" navbar>
                             <NavItem>
+                                <NavLink href="/">Login</NavLink>
+                                <NavLink  href="/">Sign In</NavLink>
                             </NavItem>
                         </Nav>
                     </Container>
                 </Navbar>
+            </div>
             
         //         <form onSubmit={this.handleSubmit}>
         //   <FormGroup controlId="email" bsSize="large">
