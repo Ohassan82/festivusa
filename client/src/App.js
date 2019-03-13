99f4bb92bad4385da2a28c0f7f73d5c236c847da
Sam Matthiae <rocketsam13@yahoo.com>
2019-03-13 14:46:22 -0500

    back to auth0

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,74 @@
 import React, { Component } from 'react';
-//import { Navbar, Button } from 'react-bootstrap';
-import "bootstrap/dist/css/bootstrap.min.css";
-import Register from "./component/register/Register";
-import Login from "./component/login/Login";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Events from "./Events";
-import FirstPage from "./pages/FirstPage";
-
+import { Navbar, Button } from 'react-bootstrap';
+import './App.css';
 
 class App extends Component {
+  goTo(route) {
+    this.props.history.replace(`/${route}`)
+  }
+
+  login() {
+    this.props.auth.login();
+  }
+
+  logout() {
+    this.props.auth.logout();
+  }
+
+  componentDidMount() {
+    const { renewSession } = this.props.auth;
+
+    if (localStorage.getItem('isLoggedIn') === 'true') {
+      renewSession();
+    }
+  }
 
   render() {
+    const { isAuthenticated } = this.props.auth;
 
     return (
-    
-        <Router>
-          <div>
-            <Login />
-            <div>
-            <Register />
-            </div>
-            <Switch>
-                <Route exact path="/" component={FirstPage} />
-                <Route exact path="/search" component={Events} />
-            </Switch>
-          </div>
-        </Router>
-
+      <div>
+        <Navbar fluid>
+          {/* <Navbar.Header> */}
+            <Navbar.Brand>
+              <a href="/">Auth0 - React</a>
+            </Navbar.Brand>
+            <Button
+              bsStyle="primary"
+              className="btn-margin"
+              onClick={this.goTo.bind(this, 'home')}
+            >
+              Home
+            </Button>
+            {
+              !isAuthenticated() && (
+                  <Button
+                    id="qsLoginBtn"
+                    bsStyle="primary"
+                    className="btn-margin"
+                    onClick={this.login.bind(this)}
+                  >
+                    Log In
+                  </Button>
+                )
+            }
+            {
+              isAuthenticated() && (
+                  <Button
+                    id="qsLogoutBtn"
+                    bsStyle="primary"
+                    className="btn-margin"
+                    onClick={this.logout.bind(this)}
+                  >
+                    Log Out
+                  </Button>
+                )
+            }
+          {/* </Navbar.Header> */}
+        </Navbar>
+      </div>
     );
   }
 }
 
- export default App;
+export default App;
---
+++ client/src/Auth/auth0-variables.js
@@ -0,0 +1,10 @@
+// export const AUTH_CONFIG = {
+//   domain: 'rocket13.auth0.com',
+//   clientId: 'MdQshL5NmARU7XQBUNmFDSZsms1lHGw_',
+//   callbackUrl: 'http://localhost:3000/callback'
+// }
+export const AUTH_CONFIG = {
+  domain: 'festivusa.auth0.com',
+  clientId: 'wzKPqLjHVmrXxCP8HewrBG42byfWiaM9',
+  callbackUrl: 'http://localhost:3000/callback'
+}
