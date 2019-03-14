0375857d3320adc93afd058b505e2ee650f9fec2
Sam Matthiae <rocketsam13@yahoo.com>
2019-03-14 15:01:11 -0500

    fixing heroku connection

--- server.js
+++ server.js
@@ -12,9 +12,11 @@ const path = require("path")
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
@@ -24,11 +26,13 @@ if (process.env.NODE_ENV === "production") {
 // Send every other request to the React app
 // Define any API routes before this runs
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  res.sendFile(path.join(__dirname, "./client/public/index.html"));
 });
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://heroku_8d4vh4fb:eh6saeqn67oao5ig4f5vk1er2c@ds149365.mlab.com:49365/heroku_8d4vh4fb") 
+mongoose.connect(process.env.MONGODB_URI || "mongodb://<dbuser>:<dbpassword>@ds149365.mlab.com:49365/heroku_8d4vh4fb") 
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
+
+//mongodb://heroku_8d4vh4fb:eh6saeqn67oao5ig4f5vk1er2c@ds149365.mlab.com:49365/heroku_8d4vh4fb
