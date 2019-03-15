e593387928e488c0f1494f8d7732f86e1ac4c957
Sam Matthiae <rocketsam13@yahoo.com>
2019-03-15 15:28:37 -0500

    search is stayin on page, but wont work with image on page

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Navbar, Button } from 'react-bootstrap';
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
+//import "./App.css";
 
 
 
